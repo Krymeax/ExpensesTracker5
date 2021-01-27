aced96c20373c504255936674bd84270e85393d9
Krymeax <57404778+Krymeax@users.noreply.github.com>
2021-01-27 21:26:39 +0200

    Fixed/added pie chart functionality in the charts tab. Connected IsCompleted with a checkbox (still need to add visibility functionality for the expense date and due date)

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -1,5 +1,6 @@
 ﻿using ExpensesTracker5.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -20,7 +21,8 @@ namespace ExpensesTracker5.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<ExpenseModel> objList = _db.Expenses;
+            var objList = _db.Expenses.ToList();
+            //Console.WriteLine(JsonConvert.SerializeObject(objList))
             return View(objList);
         }
 
@@ -37,7 +39,7 @@ namespace ExpensesTracker5.Controllers
             if (!String.IsNullOrEmpty(searchString))
             {
                 IEnumerable<ExpenseModel> expensesList = GetSearchResult(searchString).ToList();
-                return View("Index",expensesList);
+                return View("Index", expensesList);
             }
             else return View("Index", objList);
         }
@@ -46,6 +48,7 @@ namespace ExpensesTracker5.Controllers
         public IActionResult Create()
         {
             IEnumerable<ExpenseModel> objList = _db.Expenses;
+
             return View();
         }
 
@@ -54,21 +57,23 @@ namespace ExpensesTracker5.Controllers
         [Microsoft.AspNetCore.Mvc.ValidateAntiForgeryToken]
         public IActionResult Create(ExpenseModel obj)
         {
+
             // Didn't check for validation because it was bugging out
-                _db.Expenses.Add(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            //Console.WriteLine(JsonConvert.SerializeObject(obj));
+            return RedirectToAction("Index");
         }
 
         // GET EDIT
         public IActionResult Edit(int? id)
         {
-            if(id==null | id == 0)
+            if (id == null | id == 0)
             {
                 return NotFound();
             }
             var obj = _db.Expenses.Find(id);
-            if(obj == null)
+            if (obj == null)
             {
                 return NotFound();
             }
@@ -193,7 +198,8 @@ namespace ExpensesTracker5.Controllers
         {
             List<ExpenseModel> list = new List<ExpenseModel>();
             list = _db.Expenses.Select(a => new ExpenseModel { ExpenseName = a.ExpenseName, Amount = a.Amount }).ToList();
-            return Json(new { JSONList = list }, JsonRequestBehavior.AllowGet);
+            //Console.WriteLine(JsonConvert.SerializeObject(list));
+            return Json(new { JSONList = list });
         }
 
 
@@ -243,7 +249,7 @@ namespace ExpensesTracker5.Controllers
             exp = _db.Expenses.ToList();
             result = exp.Where(x => x.ExpenseName.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) != -1);
             if (result.Any() == false)
-            result = exp.Where(x => x.Category.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) != -1);
+                result = exp.Where(x => x.Category.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) != -1);
             if (result.Any() == false)
             {
                 IEnumerable<ExpenseModel> expenseDates = null;
@@ -254,7 +260,7 @@ namespace ExpensesTracker5.Controllers
             }
             if (result.Any() == false && (searchString == "Expired" || searchString == "expired" || searchString == "EXPIRED"))
             {
-                result = exp.Where(x => x.Expired == true );
+                result = exp.Where(x => x.Expired == true);
             }
             if (result.Any() == false && (searchString == "due" || searchString == "Due" || searchString == "DUE"))
             {
